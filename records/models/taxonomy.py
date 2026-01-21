9bc917d4393d43635a78e8989b02d322f744b36f
Joe <joekbullard@gmail.com>
2026-01-21 21:54:11 +0000

    Add plural name to model

--- records/models/taxonomy.py
+++ records/models/taxonomy.py
@@ -1,19 +1,22 @@
 """This contains taxonomy info for bat species"""
 from django.db import models
 
+class Rank(models.TextChoices):
+    SPECIES = "species", "Species"
+    GENUS = "genus", "Genus"
+    GROUP = "functional", "Functional"
+    UNKNOWN = "unknown", "Unknown"  
+
 class Taxon(models.Model):
-    
-    class Rank(models.TextChoices):
-        SPECIES = "species", "Species"
-        GENUS = "genus", "Genus"
-        GROUP = "functional", "Functional"
-        UNKNOWN = "unknown", "Unknown"
-        
     common_name = models.CharField(max_length=100)
     scientific_name = models.CharField(max_length=100)
-    rank = models.TextChoices(choices=Rank.choices)
+    rank = models.CharField(choices=Rank.choices)
     genus = models.CharField(max_length=50)
     nbn_taxon_id = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = "taxa"
+    
     def __str__(self):
         return f"{self.common_name}"
+    
