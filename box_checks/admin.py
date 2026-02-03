d6e0463ec62af8ba78680884893c3994291f2127
Joe <joekbullard@gmail.com>
2026-02-03 23:20:48 +0000

    Adds box check models to admin

--- box_checks/admin.py
+++ box_checks/admin.py
@@ -1,8 +1,8 @@
 from django.contrib.gis import admin
-from box_checks.models import Site, Box, Inspection
+from box_checks.models import Site, Box, Inspection, Survey
 
 admin.site.register(Box, admin.GISModelAdmin)
 admin.site.register(Inspection, admin.ModelAdmin)
-admin.site.register(Site, admin.ModelAdmin)
-
+admin.site.register(Site, admin.GISModelAdmin)
+admin.site.register(Survey, admin.ModelAdmin)
 
